30c4f02e94f62cd7acd336c504cc4b3d59291893
Darginec05 <Devopsbanda@gmail.com>
2018-03-13 18:53:39 +0100

    finish

--- src/controller.js
+++ src/controller.js
@@ -1,32 +1,3 @@
-// class Controller{
-//     constructor(model, view){
-//         this.model = model;
-//         this.view = view;
-
-//         view.on('add', this.addTodo.bind(this));
-//     }
-
-//     addTodo(title){
-//         const item = this.model.addItem({
-//             id: Date.now(),
-//             title: title,
-//             completed: false
-//         });
-
-//         this.view.addItem(todo);
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
 class Controller {
     constructor(model, view) {
         this.model = model;
